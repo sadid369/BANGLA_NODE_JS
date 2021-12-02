3affaf16f368ac8dcbce64dcfc55a3f3e6ffd132
Sadid369 <sadid.jones@gmail.com>
2021-12-02 22:25:35 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,19 @@
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const div = (a, b) => a / b;
+const test = (a, b) => add(a, b) / sub(a, b);
+
+// module.exports = test;
+
+// module.exports = {
+//   test,
+//   add,
+//   sub,
+//   div,
+// };
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.div = div;
+// module.exports.test = test;
+console.log(module);
